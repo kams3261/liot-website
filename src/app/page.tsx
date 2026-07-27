4445f73878d23267205285f529fc0ff930dc433a
S.Kamada <syuhei.k1118@gmail.com>
2026-07-27 17:07:47 +0900

    2秒のタイマー追加（loading確認用）

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
-export default function Home() {
+export default async function Home() {
+  await new Promise((r) => setTimeout(r, 2000));
+
   return (
     <div className="mx-auto flex w-full flex-1 items-center justify-center px-4 py-10 text-center sm:px-6 sm:py-16">
       <p>Test Website Hello World.<br />
