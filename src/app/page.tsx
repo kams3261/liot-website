43138578b6c40f7dfe955ee8fa5c728dd7a4d014
S.Kamada <syuhei.k1118@gmail.com>
2026-07-27 17:30:26 +0900

    Fragmentの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,20 @@ export default async function Home() {
   // await new Promise((r) => setTimeout(r, 5000));
 
   return (
-    <div className="mx-auto flex w-full flex-1 items-center justify-center py-10 text-center sm:py-16">
+    <>
+      <div className="mx-auto flex w-full flex-1 items-center justify-center py-10 text-center sm:py-16">
+        <p>Test Website Hello World.<br />
+          ローカルで確認：http://localhost:3000/<br />
+          本番ページのURL：https://liot-website.vercel.app/<br />
+          Vercelの管理画面：https://vercel.com/syuheik1118-gmailcoms-projects/liot-website</p>
+      </div>
+
+      <div className="mx-auto flex w-full flex-1 items-center justify-center py-10 text-center sm:py-16">
       <p>Test Website Hello World.<br />
         ローカルで確認：http://localhost:3000/<br />
         本番ページのURL：https://liot-website.vercel.app/<br />
         Vercelの管理画面：https://vercel.com/syuheik1118-gmailcoms-projects/liot-website</p>
-    </div>
+      </div>
+    </>
   );
 }
