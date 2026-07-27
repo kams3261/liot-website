c26f9e5ca87bc0c18fa00931433e2b2f5e47a18c
S.Kamada <syuhei.k1118@gmail.com>
2026-07-27 17:05:05 +0900

    テキスト修正追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ export default function Home() {
   return (
     <div className="mx-auto flex w-full flex-1 items-center justify-center px-4 py-10 text-center sm:px-6 sm:py-16">
       <p>Test Website Hello World.<br />
-        本番ページのURL：https://liot-website.vercel.app/</p>
+        本番ページのURL：https://liot-website.vercel.app/<br />
+        Vercelの管理画面：https://vercel.com/syuheik1118-gmailcoms-projects/liot-website</p>
     </div>
   );
 }
