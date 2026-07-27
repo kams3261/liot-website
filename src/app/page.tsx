304dce4d7cdaec7a1d91661caead40e759c857e8
S.Kamada <syuhei.k1118@gmail.com>
2026-07-27 15:20:43 +0900

    TEST TEXTの表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-full flex-1 items-center justify-center">
       <h1 className="text-3xl font-semibold">Hello World.</h1>
+      <p>TEST TEXTはこちら</p>
     </main>
   );
 }
