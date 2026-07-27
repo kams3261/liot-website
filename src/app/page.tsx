17d723da59b4a172d5cb0d1b24fb20ea02bcd286
S.Kamada <syuhei.k1118@gmail.com>
2026-07-27 17:31:57 +0900

    テキスト修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,7 @@ export default async function Home() {
       </div>
 
       <div className="mx-auto flex w-full flex-1 items-center justify-center py-10 text-center sm:py-16">
-      <p>Test Website Hello World.<br />
-        ローカルで確認：http://localhost:3000/<br />
-        本番ページのURL：https://liot-website.vercel.app/<br />
-        Vercelの管理画面：https://vercel.com/syuheik1118-gmailcoms-projects/liot-website</p>
+      <p>2つ目のセクション（returnの直下にdivを２つ並べることはできない。Fragmentの追加が必要）</p>
       </div>
     </>
   );
