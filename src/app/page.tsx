d7e92eded4630489a553cf366555f0c5a5bd8927
S.Kamada <syuhei.k1118@gmail.com>
2026-08-01 14:56:58 +0900

    CSS調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
           Vercelの管理画面：https://vercel.com/syuheik1118-gmailcoms-projects/liot-website</p>
       </div>
 
-      <div className="mx-auto flex w-full max-w-7xl flex-1 items-center justify-center py-10 text-center sm:py-16">
+      <div className="mx-auto w-full max-w-7xl flex-1 items-center justify-center py-10 text-center sm:py-16">
         <p>2つ目のセクション（returnの直下にdivを２つ並べることはできない。Fragmentの追加が必要）</p>
         <p>※Fragment・・・＜＞〜＜＞で挟む。</p>
       </div>
