2ca9e6db749e224292f021a3a6df5a165fc8e4b9
S.Kamada <syuhei.k1118@gmail.com>
2026-08-01 14:54:14 +0900

    テキスト修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default async function Home() {
 
       <div className="mx-auto flex w-full max-w-7xl flex-1 items-center justify-center py-10 text-center sm:py-16">
         <p>2つ目のセクション（returnの直下にdivを２つ並べることはできない。Fragmentの追加が必要）</p>
-        <p>※Fragment・・・<>〜</>で挟む。</p>
+        <p>※Fragment・・・＜＞〜＜＞で挟む。</p>
       </div>
     </>
   );
